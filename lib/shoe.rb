53a7cce30d791d812eb68f0a763a5d1cef994e9c
Nadia Virjee <nadvirjee@gmail.com>
2023-01-31 11:08:25 -0500

    commit tree 4b5418a556cc3ed935b229772335f691401f4c47

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+require 'pry';
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        self.brand = brand
+        self.color = color
+        self.size = size
+        self.material = material
+        self.condition = "old"
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
